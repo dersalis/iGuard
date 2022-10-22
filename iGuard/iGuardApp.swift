35338e4bc3a84397d4058ff0cd49c6678eeeb2e8
Damian <hello@damianruta.pl>
2022-10-22 17:07:05 +0200

    Initial Commit

---
+++ iGuard/iGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGuardApp.swift
+//  iGuard
+//
+//  Created by Damian Ruta on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
